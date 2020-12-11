142a716395289733347723cdb6e0fe863b67d104
Paulo Yun <paulo.yun@ifood.com.br>
2020-12-11 01:32:19 -0300

    node.js introduction

---
+++ intro-to-node/index.js
@@ -0,0 +1,17 @@
+
+// jshint esversion:6
+const express = require("express");
+
+const app = express()
+
+app.get()
+
+app.listen(3000, function(){
+    console.log('Server started on port 3000')
+}
+)
+// fs.copyFileSync('file1.txt','file2.txt')
+
+// var superheroes = require('superheroes');
+
+// console.log(superheroes.random())
